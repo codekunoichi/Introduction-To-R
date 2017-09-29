3c6e4a9feac7cdea172b5b5ce6983c4e62ea8a40
codekunoichi <giri.rumpa2017@gmail.com>
2017-09-29 10:02:41 -0700

    more tricks on row and column manipulation of DF

--- dataframe.R
+++ dataframe.R
@@ -61,3 +61,56 @@ head(theDF, n=7) #n is how many rows to print
 #to review the structure of data at the tail end, use the "tail" function
 tail(theDF)
 tail(theDF, n=7)
+
+theDF
+
+#individual column from the data frame
+theDF$Sport
+
+#getting the individual cell item
+theDF[3, 2] # 3rd row, 2nd column
+
+# If we want to get 3rd row second through 3rd column.
+theDF[3, 2:3]
+
+# If we want to get 3rd and 5th row 2nd column elements
+theDF[c(3, 5), 2]
+
+#you can get multiple rows and multiple columns
+theDF[c(3, 5), 2:3]
+
+#get an entire column say the third column
+# one column - returned as a vector
+theDF[, 3] # no column selected only specifies the row number
+
+#get both 2nd and 3rd column as a data frame all rows
+# two columns returned as a vector
+theDF[, 2:3]
+
+class(theDF[, 3])
+
+# to not get a vector when acking for a single column but preserve as a dataframe
+theDF[, 3, drop=FALSE]
+class(theDF[, 3, drop=FALSE])
+
+#get the second row out the DF - this comes out as a data frame
+theDF[2,]
+
+class(theDF[2,])
+
+#multiple rows out of the dataframe
+theDF[2:3,]
+
+#multiple columns can be obtained in any order by using the column names
+theDF[, c("Second", "First")]
+
+theDF[, c("First", "Second")]
+theDF[, "Sport"]
+
+theDF[, "Sport", drop=FALSE]
+
+#short cut for above  : to get a specific column as a dataframe
+theDF["Sport"]
+
+#short cut to get a column as a vector
+theDF[["Sport"]]
