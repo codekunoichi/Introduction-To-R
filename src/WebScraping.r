ab3ce5eb56da5419d3b8f448c62656ea11d13837
codekunoichi <giri.rumpa2017@gmail.com>
2017-09-29 13:00:37 -0700

    Learn how to fo Web Scraping R

---
+++ src/WebScraping.r
@@ -0,0 +1,6 @@
+# How to read HTML File and get to the first table with no header and load into a data frame with string data and not factors
+require(XML)
+theURL <- "https://www.jaredlander.com/2012/02/another-kind-of-super-bowl-pool/"
+
+bowlGame <- readHTMLTable(theURL, which=1, header=FALSE, stringsAdFactors=FALSE)
+bowlGame
