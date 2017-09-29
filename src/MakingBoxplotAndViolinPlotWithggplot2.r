e50c02ee03efeb8c7bc48535e66cdc1e02a3d702
codekunoichi <giri.rumpa2017@gmail.com>
2017-09-29 16:13:50 -0700

    Learn how to make boxplots, violin plots using ggplot2

---
+++ src/MakingBoxplotAndViolinPlotWithggplot2.r
@@ -0,0 +1,21 @@
+# Make boxplot and violin plot with ggplot2
+
+#box plot
+ggplot(diamonds, aes(y=carat, x= 1)) + geom_boxplot()
+ggplot(diamonds, aes(y=carat, x=cut)) +geom_boxplot()
+
+#violin plot
+ggplot(diamonds, aes(y=carat, x=cut)) +geom_violin()
+
+g <- ggplot(diamonds, aes(y=carat, x=cut)) 
+g
+g+ geom_violin()
+
+#The order the layers are added matters - violing first, point will be overlayed on top of it.
+g + geom_violin() + geom_point()
+
+# The points will be behind the violin plot
+g + geom_point() + geom_violin()
+
+g + geom_jitter() + geom_violin()
+
