bc658a49e2cc74104248290fc1a8b4cd4a068a9e
codekunoichi <giri.rumpa2017@gmail.com>
2017-09-29 10:31:08 -0700

    add

--- list.r
+++ list.r
@@ -20,3 +20,56 @@ list4
 
 list5 <-list(theDF, 1:10, list3)
 list5
+
+#name the list elements
+names(list5)
+names(list5) <- c("data.frame", "vector", "list")
+list5
+
+#create the list with the names of element
+list6 <-list(TheDataFrame=theDF, TheVector=1:10, TheList=list3)
+names(list6)
+
+list6
+
+#usually creating the empty list and add things later.
+emptylist <- vector(mode="list", length=4)
+emptylist
+
+#set the first element
+emptylist[[1]] <- 5
+emptylist
+
+#subset of list
+list5[[1]]
+names(list5)
+
+#one can use the names of the index instead of the index to retrieving by numeric index
+list5[["data.frame"]]
+
+#the dot notation is allowed so once the data frame arrives one may proceed with $ sign to fetch more attributes
+class(list5[["data.frame"]])
+
+list5[["data.frame"]]$Sport
+list5[[1]]$Sport
+list5[[1]]
+
+#don't forget to put quotes around the column names this returns the vector
+list5[[1]][, "Second"]
+
+#to preseve the above as a dataframe
+list5[[1]][, "Second", drop=FALSE]
+
+#length of the list
+length(list5)
+NROW(list5)
+
+#to add another element in the list
+list5
+list5[[4]] <- 2
+
+list5[["NewElement"]] <- 3:6
+
+names(list5)
+#CAUTION!! Usually extending list length after the specifying the length is frowned upon
+#hence avoid it if possible
