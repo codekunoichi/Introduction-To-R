ef79d49b0ca3167a15a9a45692fc2ebfa571cb7f
codekunoichi <giri.rumpa2017@gmail.com>
2017-09-29 12:25:44 -0700

    Learn to read data out of other statistical packages using foreign package

---
+++ src/ReadDataFilesFromOtherStatisticalTools.r
@@ -0,0 +1,16 @@
+# to read data out of other statistical packages use foreign package
+
+require(foreign)
+
+#to read from spss use
+read.spss
+
+#to read from strata
+read.dta
+
+#to read from SAS
+read.ssd # note you need to have a valid license to read SAS data, using Revolution R you might not need SAS license
+
+read.octave
+read.mtp
+read.systat
