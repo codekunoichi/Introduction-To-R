67b494ae2954db29c67bf549a50d0600176eb544
codekunoichi <giri.rumpa2017@gmail.com>
2017-10-02 15:39:06 -0700

    Learn aggregate function.

---
+++ src/LearnAggregate.r
@@ -0,0 +1,13 @@
+require(ggplot2)
+data(diamonds)
+head(diamonds)
+mean(diamonds$price)
+
+
+aggregate(price ~ cut, diamonds, mean)
+
+aggregate(price ~ cut + color, diamonds, mean)
+
+aggregate(cbind(price, carat) ~ cut, diamonds, mean)
+
+aggregate(cbind(price, carat) ~ cut +color, diamonds, mean)
