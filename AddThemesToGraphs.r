118e358559e92f779527861bfa42b302e3c60720
codekunoichi <giri.rumpa2017@gmail.com>
2017-10-02 08:45:49 -0700

    Explore/Learn theme for graphs

--- AddThemesToGraphs.r
+++ AddThemesToGraphs.r
@@ -2,4 +2,17 @@
 require(ggplot2)
 require(ggthemes)
 
-g < ggplot(diamonds, aes(x=carat, y=price, color=color))
+g <- ggplot(diamonds, aes(x=carat, y=price, color=color)) + geom_point()
+g
+
+#add WSJ Theme
+g + theme_wsj()
+
+#add economist theme
+g + theme_economist() + scale_colour_economist()
+
+#minimalist edward tufte
+g + theme_tufte()
+
+# to give appearance of graph came from excel
+g + theme_excel() + scale_color_excel()
