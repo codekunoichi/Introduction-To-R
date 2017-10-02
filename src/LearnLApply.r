3be1abc87359d3f6863f40a24a8dfa1b65c796ea
codekunoichi <giri.rumpa2017@gmail.com>
2017-10-02 15:26:34 -0700

    Learn apply, sapply, mapply.

--- src/LearnLApply.r
+++ src/LearnLApply.r
@@ -5,3 +5,25 @@ theList
 lapply(theList, sum)
 
 sapply(theList, sum)
+
+theNames <- c("George", "Lisa", "Baron")
+# lapply, sapply works on vector as well
+lapply(theNames, nchar)
+
+#mapply - goes over multiple list for the same function, element by element.
+
+firstList <- list(A=matrix(1:16, 4), b=matrix(1:16, 2), c=1:5)
+secondList <- list(A=matrix(1:16, 4), b=matrix(1:16, 8), c=15:1)
+firstList
+
+secondList
+
+#check to see if each argument 
+mapply(identical, firstList, secondList)
+
+simplefunc <- function(x, y)
+{
+  NROW(x) + NROW(y)
+}
+
+mapply(simplefunc, firstList, secondList)
